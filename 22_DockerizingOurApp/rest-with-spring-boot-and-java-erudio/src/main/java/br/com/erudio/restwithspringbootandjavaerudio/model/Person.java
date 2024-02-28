bf74b995f2dbde827c48ee6ffee6fb4f982466f8
MarianaConsolaro <116835308+MarianaConsolaro@users.noreply.github.com>
2024-02-28 15:44:00 -0300

    Add files via upload

---
+++ 22_DockerizingOurApp/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/restwithspringbootandjavaerudio/model/Person.java
@@ -0,0 +1,109 @@
+package br.com.erudio.restwithspringbootandjavaerudio.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "person")
+public class Person implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name", nullable = false, length = 80)
+    private String firstName;
+    @Column(name = "last_name", nullable = false, length = 80)
+    private String lastName;
+    @Column(nullable = false, length = 100)
+    private String address;
+    @Column(nullable = false, length = 6)
+    private String gender;
+
+    @Column(nullable = false)
+    private Boolean enabled;
+
+
+    public Person() {} // Constructor padrão que o JPA exige.
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Person person))
+            return false;
+        if (!id.equals(person.id))
+            return false;
+        if (!firstName.equals(person.firstName))
+            return false;
+        if (!lastName.equals(person.lastName))
+            return false;
+        if (!address.equals(person.address))
+            return false;
+        return gender.equals(person.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        result = 31 * result + address.hashCode();
+        result = 31 * result + gender.hashCode();
+        return result;
+    }
+}
